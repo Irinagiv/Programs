c494b9b9fa071bf3835e07dd6cb255ded0c58b30
Irinagiv <irisha.guryeva@gmail.com>
2013-02-16 16:36:16 +0600

    exercise1 has been added
    Signed-off-by: Irinagiv <irisha.guryeva@gmail.com>

---
+++ Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double s1 = AreaSubPrograms.RectangleAreaProcedure();
+            double s2 = AreaSubPrograms.HollowRectangleAreaProcedure();
+
+            bool hit = HitSubPrograms.HitRectangle();
+            bool hitDash = HitSubPrograms.HitHollowRectangle();
+        }
+    }
+}
